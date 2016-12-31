9210502cd2d8e4d8f545385049b9e87d89a90887
Yac836 <374012581@qq.com>
2016-12-31 21:01:34 +0800

    add NumberofSegmentsinaString

---
+++ NumberofSegmentsinaString/NumberofSegmentsinaString.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
+using namespace std;
+int countSegments(string s) {
+	vector<string> tokens;
+	istringstream  iss(s);
+	copy(istream_iterator<string>(iss),
+		istream_iterator<string>(),
+		back_inserter(tokens));
+	return tokens.size();
+
+}
+
+int main() {
+	string test = "hello      zhao hong yan";
+cout <<	countSegments(test);
+	
+
+}
