86c09ebfdd3d2dff447c6c2708e4087100d5fe15
Yac836 <374012581@qq.com>
2020-03-19 09:51:16 +0800

    ExcelSheetColumnNumber

--- ExcelSheetColumnNumber.h
+++ ExcelSheetColumnNumber.h
@@ -6,3 +6,17 @@
 #define LEETCODE_EXCELSHEETCOLUMNNUMBER_H
 
 #endif //LEETCODE_EXCELSHEETCOLUMNNUMBER_H
+
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int res = 0;
+        for(auto e : s){
+            int i = e - 'A'  + 1;
+            res = res * 26 + i;
+        }
+        return res;
+    }
+};
