2fdd4e7c3dac49218ef3c5d58807dca1a49a7c71
Yac836 <374012581@qq.com>
2020-06-13 19:59:06 +0800

    '70'

---
+++ 70.climbStairs.cpp
@@ -0,0 +1,16 @@
+//
+// Created by zhaohongyan on 2020/6/13.
+//
+
+int climbStairs(int n) {
+    if(n <=2)
+        return n;
+    int  a = 1, b =2;
+    for(int i = 3; i <= n; i++){
+        int t = b;
+        b = a + b;
+        a = t;
+
+    }
+    return b;
+}
