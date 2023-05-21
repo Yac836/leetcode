b66539a435b235ec6c9eee53dbc0bc3f52bb5916
Yac836 <374012581@qq.com>
2023-05-21 22:25:04 +0800

    1073

---
+++ 1073.storeWater.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    int storeWater(vector<int> &bucket, vector<int> &vat) {
+        int n = bucket.size();
+        int maxk = *max_element(vat.begin(), vat.end());
+        if (maxk == 0) {
+            return 0;
+        }
+        int res = INT_MAX;
+        for (int k = 1; k <= maxk && k < res; ++k) {
+            int t = 0;
+            for (size_t i = 0; i < bucket.size(); i++) {
+                t += max(0, (vat[i] + k - 1) / k - bucket[i]);
+            }
+            res = min(res, t + k);
+        }
+        return res;
+    }
+};
+int main() {
+    system("pause");
+    return 0;
+}
