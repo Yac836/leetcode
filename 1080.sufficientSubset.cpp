d1e8f323fd3fe7f84667ac892b446deb89348430
Yac836 <374012581@qq.com>
2023-05-23 22:42:13 +0800

    1080

---
+++ 1080.sufficientSubset.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#include "common_head.h"
+using namespace std;
+class Solution {
+public:
+    bool checkSufficientLeaf(TreeNode *node, int sum, int limit) {
+        if (!node) {
+            return false;
+        }
+        if (node->left == nullptr && node->right == nullptr) {
+            return node->val + sum >= limit;
+        }
+        bool haveSufficientLeft = checkSufficientLeaf(node->left, sum + node->val, limit);
+        bool haveSufficientRight = checkSufficientLeaf(node->right, sum + node->val, limit);
+        if (!haveSufficientLeft) {
+            node->left = nullptr;
+        }
+        if (!haveSufficientRight) {
+            node->right = nullptr;
+        }
+        return haveSufficientLeft || haveSufficientRight;
+    }
+    TreeNode *sufficientSubset(TreeNode *root, int limit) {
+        bool haveSufficient = checkSufficientLeaf(root, 0, limit);
+        return haveSufficient ? root : nullptr;
+    }
+};
+
+TreeNode *sufficientSubset(TreeNode *root, int limit) {
+}
+int main() {
+    return 0;
+}
