153ff9e689e95de10c2a34f08affe33b61a39657
374012581@qq.com <work@localhost.localdomain>
2023-11-12 11:06:44 +0800

    200

---
+++ unionfind.cpp
@@ -0,0 +1,52 @@
+// 注意：cpp 代码由 chatGPT🤖 根据我的 java 代码翻译，旨在帮助不同背景的读者理解算法逻辑。
+// 本代码不保证正确性，仅供参考。如有疑惑，可以参照我写的 java 代码对比查看。
+
+class UF {
+private:
+    // 连通分量个数
+    int count;
+    // 存储每个节点的父节点
+    int *parent;
+
+public:
+    // n 为图中节点的个数
+    UF(int n) {
+        this->count = n;
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+    
+    // 将节点 p 和节点 q 连通
+    void union_(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        
+        if (rootP == rootQ)
+            return;
+        
+        parent[rootQ] = rootP;
+        // 两个连通分量合并成一个连通分量
+        count--;
+    }
+
+    // 判断节点 p 和节点 q 是否连通
+    bool connected(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        return rootP == rootQ;
+    }
+
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    // 返回图中的连通分量个数
+    int count_() {
+        return count;
+    }
+};
